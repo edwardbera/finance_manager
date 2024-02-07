de0c030a94328f2734935b950531182de371870c
Edward Bera <47110064+edwardbera@users.noreply.github.com>
2024-02-08 00:37:00 +0200

    Creating sqlite database with income and expenditure tables

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module finance_manager
 
 go 1.21.0
 
-require github.com/Rhymond/go-money v1.0.10 // indirect
+require (
+	github.com/Rhymond/go-money v1.0.10 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+)
--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type income struct {
@@ -37,15 +41,90 @@ func addIncome() *income {
 
 	fmt.Println("Enter the amount :")
 	amount, err3 := reader.ReadString('\n')
-	myincome.amount, err = strconv.ParseFloat(amount, 32)
-
+	amount = strings.TrimSpace(amount)
+	myincome.amount, err = strconv.ParseFloat(amount, 64)
 	if err3 != nil {
 		log.Fatal(err)
 	}
 
+	myincome.date = time.Now()
+	insertData(&myincome)
 	return &myincome
 }
 
-func main() {
+func addExpense() *income {
+	var myincome = income{}
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the Expense :")
+	title, err := reader.ReadString('\n')
+	myincome.title = title
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Enter the description :")
+	description, err2 := reader.ReadString('\n')
+	myincome.description = description
+
+	if err2 != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Enter the amount :")
+	amount, err3 := reader.ReadString('\n')
+	amount = strings.TrimSpace(amount)
+	myincome.amount, err = strconv.ParseFloat(amount, 64)
+	if err3 != nil {
+		log.Fatal(err)
+	}
+
+	myincome.date = time.Now()
+	insertData(&myincome)
+	return &myincome
+}
+
+func createDB() {
+	db, err := sql.Open("sqlite3", "database.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	sts := `
+	CREATE TABLE IF NOT EXISTS income(id INTEGER PRIMARY KEY, title TEXT, description TEXT, date TEXT, amount Decimal);
+	`
+	sts2 := `
+	CREATE TABLE IF NOT EXISTS expense(id INTEGER PRIMARY KEY, title TEXT, description TEXT, date TEXT, amount Decimal);
+	`
+	_, err = db.Exec(sts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err2 := db.Exec(sts2)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println("Database created.")
+}
+
+func insertData(data *income) {
+	db, err := sql.Open("sqlite3", "database.db")
+	if err != nil {
+		panic(err)
+	}
+
+	sts := "INSERT INTO income(title, description, date, amount) VALUES ('" + data.title + "', '" + data.description + "', '" + data.date.String() + "'," + strconv.FormatFloat(data.amount, 'f', 2, 64) + " )"
+
+	_, err = db.Exec(sts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
+
+func main() {
+	createDB()
+	//addIncome()
 }
